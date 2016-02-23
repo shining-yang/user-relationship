af6482bbee42ed6b2089f1d2971be3806c2ca1b0
Shining Yang <y.s.n@live.com>
2016-02-23 22:27:00 +0800

    minor fix: repeated "liked"

--- init.sql
+++ init.sql
@@ -17,40 +17,40 @@ BEGIN
     SELECT INTO my_prev_state R.state FROM relationships AS R WHERE R.id=arg_id AND R.other_id=arg_other_id;
     SELECT INTO other_prev_state R.state FROM relationships AS R WHERE R.id=arg_other_id AND R.other_id=arg_id;
 	IF arg_state = 'liked' THEN
-		IF my_prev_state is null THEN	--我方条目尚未存在
-			IF other_prev_state = 'liked' THEN	--对方条目已经存在并且为liked
+		IF my_prev_state is null THEN  --我方条目尚未存在
+			IF other_prev_state = 'liked' THEN  --对方条目已经存在并且为liked
 				INSERT INTO relationships VALUES (arg_id, arg_other_id, 'matched');
 				UPDATE relationships AS R SET state='matched' WHERE R.id=arg_other_id AND R.other_id=arg_id;
 				final_state = 'matched';
-			ELSE	--对方条目尚未存在或不是liked
+			ELSE  --对方条目尚未存在或不是liked
 				INSERT INTO relationships VALUES (arg_id, arg_other_id, 'liked');
 				final_state = 'liked';
 			END IF;
-		ELSE	--我方条目已经存在
-			IF other_prev_state = 'liked' THEN	--对方条目已经存在并且为liked
+		ELSE  --我方条目已经存在
+			IF other_prev_state = 'liked' THEN  --对方条目已经存在并且为liked
 				UPDATE relationships AS R SET state='matched' WHERE R.id=arg_id AND R.other_id=arg_other_id;
-				UPDATE relationships AS R SET state='matched' WHERE R.id=arg_other_id AND R.other_id=arg_id; --对方条目由liked升级为matched
+				UPDATE relationships AS R SET state='matched' WHERE R.id=arg_other_id AND R.other_id=arg_id;  --对方条目由liked升级为matched
 				final_state = 'matched';
-			ELSE 	--对方条目尚未存在或不是liked
-				IF my_prev_state <> 'liked' THEN	-- 我方条目已经存在，并且不是liked
+			ELSEIF other_prev_state = 'matched' THEN  --对方条目已经存在并且为matched
+				IF my_prev_state <> 'matched' THEN  -- 我方条目已经存在，并且不是matched
+					UPDATE relationships AS R SET state='matched' WHERE R.id=arg_id AND R.other_id=arg_other_id;
+				END IF;
+				final_state = 'matched';			
+			ELSE  --对方条目尚未存在或disliked
+				IF my_prev_state <> 'liked' THEN  -- 我方条目已经存在，并且不是liked
 					UPDATE relationships AS R SET state='liked' WHERE R.id=arg_id AND R.other_id=arg_other_id;
 				END IF;
 				final_state = 'liked';
 			END IF;
 		END IF;
 	ELSEIF arg_state = 'disliked' THEN
-		IF my_prev_state is null THEN	--我方条目尚未存在
-			IF other_prev_state = 'matched' THEN	--对方条目已经存在并且为matched
-				INSERT INTO relationships VALUES (arg_id, arg_other_id, 'disliked');
-				UPDATE relationships AS R SET state='liked' WHERE R.id=arg_other_id AND R.other_id=arg_id;
-			ELSE	--对方条目尚未存在或不是matched
-				INSERT INTO relationships VALUES (arg_id, arg_other_id, 'disliked');
-			END IF;
-		ELSE	--我方条目已经存在
-			IF other_prev_state = 'matched' THEN	--对方条目已经存在并且为matched
+		IF my_prev_state is null THEN  --我方条目尚未存在
+			INSERT INTO relationships VALUES (arg_id, arg_other_id, 'disliked');
+		ELSE  --我方条目已经存在
+			IF other_prev_state = 'matched' THEN  --对方条目已经存在并且为matched
 				UPDATE relationships AS R SET state='disliked' WHERE R.id=arg_id AND R.other_id=arg_other_id;
 				UPDATE relationships AS R SET state='liked' WHERE R.id=arg_other_id AND R.other_id=arg_id;	--对方条目由matched退化为liked
-			ELSE 	--对方条目尚未存在或不是matched
+			ELSE  --对方条目尚未存在或不是matched
 				IF my_prev_state <> 'disliked' THEN 	--我方条目已经存在，并且不是disliked
 					UPDATE relationships AS R SET state='disliked' WHERE R.id=arg_id AND R.other_id=arg_other_id;
 				END IF;
